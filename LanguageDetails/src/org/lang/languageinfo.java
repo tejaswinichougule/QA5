5d3bd4579f2e949feff47bc69d7fd382f4377c2e
Admin <Admin@CSE>
2021-09-13 19:05:29 +0530

    commit

---
+++ LanguageDetails/src/org/lang/languageinfo.java
@@ -0,0 +1,21 @@
+package org.lang;
+
+public class languageinfo {
+	
+	public void tamillanguage() {
+		System.out.println("tamil");
+	}
+	public void englishlanguage() {
+		System.out.println("english");
+	}
+	public  void hindilanguage() {
+		System.out.println("hindi");		
+	}
+	
+	public static void main(String[] args) {
+		languageinfo lan=new languageinfo();
+		lan.tamillanguage();
+		lan.englishlanguage();
+		lan.hindilanguage();
+		}
+}
---
+++ LanguageDetails/src/org/lang/statedetails.java
@@ -0,0 +1,21 @@
+package org.lang;
+
+public class statedetails {
+	public void  southindia() {
+		System.out.println("south");		
+	}
+	public void northindia() {
+		System.out.println("north");
+	}
+	public static void main(String[] args) {
+		statedetails st=new statedetails();
+		st.southindia();
+		st.northindia();
+	
+	languageinfo lann=new languageinfo();
+	lann.tamillanguage();
+	lann.englishlanguage();
+	lann.hindilanguage();}
+	
+
+}
