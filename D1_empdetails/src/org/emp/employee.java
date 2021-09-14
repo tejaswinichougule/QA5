9040cbce5235a7d9b55d7d41e7918e47fb1144ab
Admin <Admin@CSE>
2021-09-14 11:13:17 +0530

    commit

---
+++ D1_empdetails/src/org/emp/employee.java
@@ -0,0 +1,39 @@
+package org.emp;
+
+public class employee {
+	
+	private void empId() {
+		System.out.println(1);
+	}
+	
+	private void empName() {
+		System.out.println("java");
+	}
+	
+	private void  empdob() {
+		System.out.println("25");
+	}
+	
+	private void empphone() {
+		System.out.println(86766767);
+	}
+	
+	private void empEmail() {
+		System.out.println("abc@gmail.com");	
+	}
+	
+private void empadd() {
+	System.out.println("pune");
+	
+}
+public static void main(String[] args) {
+	
+	employee em=new employee();
+	em.empId();
+	em.empName();
+	em.empdob();
+	em.empphone();
+	em.empEmail();
+}
+	
+	}
