c8c80c0ec6ff20f195d8a75306df5907a2f989fe
Admin <Admin@CSE>
2021-09-14 11:22:02 +0530

    commit

---
+++ D2_computer_single/src/org/single/computer.java
@@ -0,0 +1,16 @@
+package org.single;
+
+public class computer {
+	
+	public void commodel() {
+		
+		System.out.println("tab");
+		
+	}
+
+	public static void main(String[] args) {
+	
+		computer cs=new computer();
+		cs.commodel();
+	}
+}
---
+++ D2_computer_single/src/org/single/desktop.java
@@ -0,0 +1,18 @@
+package org.single;
+
+public class desktop  extends computer{
+
+	public void deksize() {
+		
+		System.out.println("12");
+		
+	}
+	
+	public static void main(String[] args) {
+		desktop dk=new desktop();
+		dk.deksize();
+		
+		dk.commodel();
+		
+	}
+}
