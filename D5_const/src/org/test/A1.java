e2720d58162c6ad5f82a333592746d3a860a9bec
Admin <Admin@CSE>
2021-09-15 10:53:56 +0530

    commit

---
+++ D5_const/src/org/test/A1.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class A1 extends B {
+	
+	public A1() {
+
+		// ass 4 and ass6
+		super(12);
+		System.out.println("default const");
+		}
+	public static void main(String[] args) {
+		 A1 a=new A1();
+	}
+	
+	
+
+}
---
+++ D5_const/src/org/test/B.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class B {
+	/*public B() {
+		System.out.println("super class");
+	}
+	*/
+	//ass5
+	public B(int id) {
+		System.out.println(id);
+		
+	
+		
+	}
+
+}
+
